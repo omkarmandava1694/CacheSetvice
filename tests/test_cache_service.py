93a196a4b06a0c8be94ab449adbf165241aaf022
Omkar Chowdary Mandava <omkarchowdarymandava@Omkars-MacBook-Pro.local>
2025-04-09 18:53:11 -0500

    Added comments for test case

--- tests/test_cache_service.py
+++ tests/test_cache_service.py
@@ -4,14 +4,17 @@ from cache.entity import Entity
 from cache.cache_service import CacheService
 
 class TestCacheService(unittest.TestCase):
+    # Setting up a cache service instance with max size = 2
     def setUp(self):
         self.cache_service = CacheService(2)
 
+    # Test adding an item and retrieving it from cache
     def test_add_and_get(self):
         e1 = Entity(1, "data1")
         self.cache_service.add(e1)
         self.assertEqual(self.cache_service.get(e1).data, "data1")
 
+    # Test for evection when the cache is full
     def test_eviction(self):
         e1 = Entity(1, "data1")
         e2 = Entity(2, "data2")
@@ -21,12 +24,14 @@ class TestCacheService(unittest.TestCase):
         self.cache_service.add(e3)
         self.assertIsNone(self.cache_service.get(e1))  # e1 should be evicted
 
+    # Test for removing single entry from the cache
     def test_remove(self):
         e1 = Entity(1, "data1")
         self.cache_service.add(e1)
         self.cache_service.remove(e1)
         self.assertIsNone(self.cache_service.get(e1))
-
+    
+    # Test case for removing all the entries from cache
     def test_remove_all(self):
         e1 = Entity(1, "data1")
         e2 = Entity(2, "data2")
@@ -35,7 +40,8 @@ class TestCacheService(unittest.TestCase):
         self.cache_service.removeAll()
         self.assertIsNone(self.cache_service.get(e1))
         self.assertIsNone(self.cache_service.get(e2))
-
+    
+    # Test case for clearing the cache
     def test_clear(self):
         e1 = Entity(1, "data1")
         self.cache_service.add(e1)
